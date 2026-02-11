a1eda8eccd24dc84785c95f3c4d04679db7ca530
SeinalArifin149 <arifinseinals3@gmail.com>
2026-02-11 21:02:14 +0700

    add titik lupa weh

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rules\Password;
-use App\Models\User
+use App\Models\User;
 
 class UserController extends Controller
 {
